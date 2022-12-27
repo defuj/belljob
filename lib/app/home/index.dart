891b47a2efc3be4daa47dfa2a2d2cf293bb1b275
Dede Fuji Abdul <defuj.official@gmail.com>
2022-12-27 16:21:13 +0700

    update prepare config language for home module

--- lib/app/home/index.dart
+++ lib/app/home/index.dart
@@ -1,3 +1,4 @@
 export 'pages/index.dart';
 export 'utils/index.dart';
 export 'widgets/index.dart';
+export 'languages/index.dart';
---
+++ lib/app/home/languages/index.dart
@@ -0,0 +1,11 @@
+import 'en.dart';
+import 'id.dart';
+
+class HomeLanguages {
+  static const Map<String, String> en = {
+    ...contentEnglish,
+  };
+  static const Map<String, String> id = {
+    ...contentIndo,
+  };
+}
